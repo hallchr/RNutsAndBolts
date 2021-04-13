e341b2730679e69b441853b11e8d7ac3b422c89c
Chris Hall <hallchr14@gmail.com>
2021-04-13 14:42:21 -0700

    added attribute names

--- RNutsAndBolts_DataTypes.R
+++ RNutsAndBolts_DataTypes.R
@@ -68,3 +68,8 @@ x <- 1:3
 names(x)
 names(x) <- c("foo", "bar", "norf")
 names(x)
+
+x<- list(a =1, b = 2, c = 3)
+m <- matrix (1:4, nrow =2, ncol = 2)
+dimnames(m) <- list(c("a", "b"), c("c", "d"))
+m
